585eee9cf4728753b03e297ccb1f23e557dc2536
deniz146 <denizfalusi99@gmail.com>
2024-07-11 12:47:07 +0300

    options menu

--- adventur of timtim/options menu.gd
+++ adventur of timtim/options menu.gd
@@ -1,10 +1,18 @@
 extends Control
 
+@onready var label = $volume/Label
+@onready var label_2 = $volume/Label2
+@onready var label_3 = $graphics/Label3
+@onready var label_4 = $graphics/Label4
+@onready var label_5 = $back/Label5
+@onready var label_6 = $back/Label6
+@onready var windowmodebutton = $windowmodebutton
+@onready var resbutton = $resbutton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	windowmodebutton.hide()
+	resbutton.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -12,7 +20,7 @@ func _process(delta):
 
 
 func _on_volume_pressed():
-	pass # Replace with function body.
+	pass
 
 
 func _on_back_pressed():
@@ -20,4 +28,35 @@ func _on_back_pressed():
 
 
 func _on_graphics_pressed():
-	pass # Replace with function body.
+	
+	windowmodebutton.show()
+	resbutton.show()
+
+func _on_volume_mouse_entered():
+	label.hide()
+	label_2.show()
+
+
+func _on_volume_mouse_exited():
+	label_2.hide()
+	label.show()
+
+
+func _on_graphics_mouse_entered():
+	label_3.hide()
+	label_4.show()
+
+
+func _on_graphics_mouse_exited():
+	label_4.hide()
+	label_3.show()
+
+
+func _on_back_mouse_entered():
+	label_5.hide()
+	label_6.show()
+
+
+func _on_back_mouse_exited():
+	label_6.hide()
+	label_5.show()
